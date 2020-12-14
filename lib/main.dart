79a79c67c278cb9e32b08155b46863d33ee5beeb
ravi2612 <ravinavarro26@gmail.com>
2020-12-14 20:56:58 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:contabilidade_Pessoal_2/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blueAccent[200],
+              child: Text('Grafico'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children:_transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2.2,                            
+                          )
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          
+                          tr.value.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,                            
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(tr.title),
+                          Text(tr.date.toString()),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
