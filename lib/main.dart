f82aa18ed5dcf3c79bf5ebfd9a7522664a0558f3
ravi2612 <ravinavarro26@gmail.com>
2020-12-15 11:39:03 -0300

    criando_novas_despesas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:contabilidade_Pessoal_2/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -35,7 +36,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -47,57 +48,84 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Column(
-            children:_transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
                           border: Border.all(
-                            color: Colors.purple,
-                            width: 2.2,                            
-                          )
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(                          
-                          tr.value.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,                            
-                          ),
+                        color: Colors.purple,
+                        width: 2.2,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$ ' + '${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),   
-                            ),
-                          Text(
-                            tr.date.toString(),
-                            style: TextStyle(
-                              color: Colors.grey[600],
-                            ),
-
-                            
-                            
-                            ),
-                        ],
-                      )
-                    ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('d MMM y').format(tr.date),
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Título'
+                    )
                   ),
-                );
-              }).toList(),
-          )
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Valor R\$ '
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      FlatButton(
+                        child: Text('Nova Transação',
+                        textScaleFactor: 1.2,),
+                        textColor: Colors.purple,
+                        onPressed: (){},
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
